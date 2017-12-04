ed44b62c826d6c89c19a6ea98479681abb893dee
jianmyphy123 <jianmyphy123@tutanota.com>
2017-12-04 09:45:14 +0100

    write comments and clean up code

--- scrapers/pipelines.py
+++ scrapers/pipelines.py
@@ -19,85 +19,6 @@ import psycopg2
 
 from scrapers.config import POSTGRE_CREDENTIALS
 
-# this class not used
-class ScrapersPipeline(object):
-    def process_item(self, item, spider):
-        return item
-
-# this class not used
-class RTEFranceFilePipeline(object):
-
-    translate_dict = {
-        "status": {
-            "Terminée": "closed",
-            "Annulée": "cancelled"
-        },
-        "type": {
-            "Indisponibilité planifiée": "planned",
-            "Indisponibilité fortuite": "unplanned",
-
-        },
-        "fuel_type": {
-            "Gaz"      : "Fossil Gas",
-            "Fioul"    : "Fossil Oil",
-            "Charbon"  : "Fossil Hard coal",
-            "Nucléaire": "Nuclear",
-            "Hydraulique STEP"        : "Hydro Pumped Storage",
-            "Hydraulique fil de l'eau / éclusée": "Hydro Run-of-river and poundage",
-            "Hydraulique lacs"        : "Hydro Water Reservoir",
-            "Autre": "Other"
-        }
-    }
-
-    header = ['version', 'type', 'pub_ts', 'status', 'unitname', 'fuel_type',
-              'begin', 'end', 'mw_cap', 'mw_available', 'comment']
-
-    def process_item(self, item, spider):
-        url = item['file_url']
-        with urllib.request.urlopen(url) as response, TemporaryFile(mode='w+b') as out_file:
-            shutil.copyfileobj(response, out_file)
-
-            df = self.dataframe_from_archive(out_file)
-            params = urllib.parse.parse_qs(getattr(urllib.parse.urlparse(url), 'query'))
-            year = params['annee'][0]
-
-            csv_fp = os.path.join(spider.scrape_dir, ''.join(['rte_france_', year, '.csv']))
-            df.to_csv(csv_fp, date_format='%Y-%m-%dT%H:%M:%SZ', index=False)
-
-    def translate_cols(self, df, translate_dict):
-        for key in translate_dict:
-            for variant in translate_dict[key]:
-                df.set_value(df[key] == variant,
-                             key, translate_dict[key][variant])
-
-    def dataframe_from_archive(self, fileobj):
-        with ZipFile(fileobj, mode='r') as archive:
-            for name in archive.namelist():
-                with archive.open(name) as csv_file, TemporaryFile(mode='w+') as csv_temp:
-                    csv_temp.write(csv_file.read().decode('windows-1252'))
-                    csv_temp.seek(0)
-                    usecols = [1, 3, 4, 5, 8, 9, 11, 13, 14, 16]
-                    report_df = pd.read_csv(
-                        csv_temp,
-                        names=self.header,
-                        sep='\t',
-                        usecols=usecols,
-                        skiprows=1,
-                        index_col=False
-                    )
-
-                    self.translate_cols(report_df, self.translate_dict)
-                    report_df['pub_ts'] = pd.to_datetime(report_df['pub_ts'],
-                                                         errors='coerce',
-                                                         format='%d/%m/%Y %H:%M')
-                    report_df['begin'] = pd.to_datetime(report_df['begin'],
-                                                         errors='coerce',
-                                                         format='%d/%m/%Y %H:%M')
-                    report_df['end'] = pd.to_datetime(report_df['end'],
-                                                         errors='coerce',
-                                                         format='%d/%m/%Y %H:%M')
-                    return report_df
-
 # pipeline that export csv file
 class CSVPipeline(object):
     """This pipeline saves items to corresponding csv files, divided by month"""
@@ -180,16 +101,6 @@ class PostgrePipeline(object):
             self.pending_items.clear()
             print("ERROR: During save to postgre:", e.pgerror)
 
-        # try:
-        #     self.update_latest(table_name=spider.table)
-        # except Exception as e:
-        #     print("Failed to set lates items")
-        #     print(e)
-        #     self.connection.rollback()
-        #     raise e
-        # finally:
-        #     self.connection.close()
-
         if self.failed_items:
             print("FAILED ITEMS:")
             for failed in self.failed_items:
@@ -199,8 +110,7 @@ class PostgrePipeline(object):
         for item in self.failed_items:
             print(item)
 
-
-        # update version number
+        # update version number for every collected event ids when spider is closing
         for event_id in self.event_ids:
             self.update_version_no(spider.table, event_id)
 
@@ -247,22 +157,6 @@ class PostgrePipeline(object):
         self.cur.execute("SELECT EXISTS(SELECT * FROM information_schema.tables WHERE table_name=%s)", (table_name,))
 
         if not (self.cur.fetchone()[0]):
-            # create_query = ('CREATE TABLE IF NOT EXISTS {0}('
-            #                 'id BIGSERIAL PRIMARY KEY,'
-            #                 'type VARCHAR(64),'
-            #                 'pub_ts TIMESTAMP WITH TIME ZONE,'
-            #                 'status VARCHAR(16),'
-            #                 'unitname VARCHAR(128),'
-            #                 'area VARCHAR(128),'
-            #                 'fuel_type VARCHAR(128),'
-            #                 'begin_ts TIMESTAMP WITH TIME ZONE,'
-            #                 'end_ts TIMESTAMP WITH TIME ZONE,'
-            #                 'mw_cap DOUBLE PRECISION,'
-            #                 'mw_available DOUBLE PRECISION,'
-            #                 'comment TEXT,'
-            #                 'last_availability BOOLEAN '
-            #                 ');'
-            #                 ).format(table_name.lower())
             create_query = ('CREATE TABLE IF NOT EXISTS {0}.{1}('
                             'id BIGSERIAL PRIMARY KEY,'
                             'type VARCHAR(64),'
@@ -286,40 +180,6 @@ class PostgrePipeline(object):
             self.connection.commit()
 
     def postgre_upsert(self, item, table_name):
-        # Performs an operation similar to 'INSERT OR UPDATE'.
-        #
-        # Not safe for concurrently using multiple siders writing to the same table:
-        # data loss possible.
-
-        # item['table'] = table_name
-
-        # insert_query = ("INSERT INTO {0} ("
-        #                 "type,"
-        #                 "pub_ts,"
-        #                 "status,"
-        #                 "unitname,"
-        #                 "area,"
-        #                 "fuel_type,"
-        #                 "begin_ts,"
-        #                 "end_ts,"
-        #                 "mw_cap,"
-        #                 "mw_available,"
-        #                 "comment"
-        #                 ") "
-        #                 "VALUES ("
-        #                 "%(type)s,"
-        #                 "%(pub_ts)s,"
-        #                 "%(status)s,"
-        #                 "%(unitname)s,"
-        #                 "%(area)s,"
-        #                 "%(fuel_type)s,"
-        #                 "%(begin)s,"
-        #                 "%(end)s,"
-        #                 "%(mw_cap)s,"
-        #                 "%(mw_available)s,"
-        #                 "%(comment)s);"
-        #                 ).format(table_name)
-
         # check if duplicated item exists
         item_exists_query = ("select id from {0}.{1} "
                                     "WHERE "
@@ -373,9 +233,11 @@ class PostgrePipeline(object):
             self.pending_items.append(item)
             self.cur.execute(insert_query, item)
 
+        # collect event_ids to update
         if item["event_id"] not in self.event_ids:
             self.event_ids.append(item['event_id'])
 
+    # the function to update version number
     def update_version_no(self, table_name, event_id):
         events_query = ("select id from {0}.{1} "
                         "where event_id="
@@ -399,48 +261,3 @@ class PostgrePipeline(object):
             version_no += 1
 
         self.connection.commit()
-
-
-    # this function not used
-    def update_latest(self, table_name):
-        """Queries through the database to set the 'latest_availability' flag."""
-
-        unique_unav_query = "SELECT DISTINCT unitname, fuel_type FROM {0};".format(table_name)
-        self.cur.execute(unique_unav_query)
-        self.connection.commit()
-
-        time_now = datetime.datetime.utcnow().isoformat()
-
-        max_subquery_template = ("(SELECT MAX(pub_ts) FROM {0} WHERE "
-                        "unitname=%(unitname)s AND "
-                        "fuel_type=%(fuel_type)s AND "
-                        "NOT begin_ts>{1})"
-                        ).format(table_name, time_now)
-
-        update_query_template = ("UPDATE {0} SET "
-                                 "latest_availability=1"
-                                 " "
-                                 "WHERE "
-                                 "unitname=%(unitname)s AND "
-                                 "fuel_type=%(fuel_type)s AND "
-                                 "pub_ts=%(subquery)s;"
-                                 ).format(table_name)
-
-        for record in self.cur.fetchall():
-            try:
-                rec_dict = {'fuel_type':record[1], 'unitname': record[0],}
-                max_subquery = self.cur.mogrify(max_subquery_template,
-                                                rec_dict)
-            except Exception as e:
-                print("Bad record:", type(record), record)
-                for it in record:
-                    print(it)
-                raise e
-            else:
-                rec_dict['subquery'] = max_subquery
-
-                try:
-                    self.cur.execute(update_query_template, rec_dict)
-                except Exception as e:
-                    print("QUERY: ", max_subquery)
-                    raise e
--- scrapers/spiders/eex_transparency_spider.py
+++ scrapers/spiders/eex_transparency_spider.py
@@ -15,8 +15,6 @@ from selenium.common.exceptions import TimeoutException
 
 from bs4 import BeautifulSoup
 
-from hashlib import sha1
-
 class EexTransparencySpider(scrapy.Spider):
     name = 'eex_availability'
     history_url_list = ['https://www.eex-transparency.com/homepage/power/austria/production/availability/non-usability-/non-usability-history-',
@@ -73,10 +71,7 @@ class EexTransparencySpider(scrapy.Spider):
         self.scraper = ScrapeJS()
         self.retrycount = 0
 
-        # mac
         self.driver = webdriver.PhantomJS('./phantomjs/mac/phantomjs')
-        # # linux
-        # self.driver = webdriver.PhantomJS('./phantomjs/linux/phantomjs')
 
         self.failed_urls = []
         self.countries = ['austria', 'belgium', 'switzerland', 'czech-republic', 'germany',
@@ -141,15 +136,6 @@ class EexTransparencySpider(scrapy.Spider):
         self.real_date = self.start
         self.cur_date = self.start
 
-        # ts_xpath = '//input[@id="to"]'
-        # try:
-        #     WebDriverWait(self.driver, 7).until(EC.presence_of_element_located((By.XPATH, ts_xpath)))
-        # except TimeoutException:
-        #     print("ERROR: Page load timeout or no data reported.")
-        #     print(self.driver.page_source)
-        #     return None
-
-        # while self.real_date < self.end and self.cur_date < (self.end + datetime.timedelta(days=5)) :
         while self.real_date <= self.end and self.cur_date <= self.end :
             print('[*] Loading page')
             page_loaded = self._load_date(self.cur_date)
@@ -207,22 +193,9 @@ class EexTransparencySpider(scrapy.Spider):
         """
         if data_object is None:
             yield
-        # "type" "short_name" "prodcon" "unit" "fuel" "connecting_area" "begin" "end" "energy_limitation" "reason" "canceled" "event_id" "modify_timestamp"
         else:
             for record in data_object:
                 item = {
-                    # 'parse_date': parse_date,
-                    # 'type': record['type'],
-                    # 'unitname': record['unit'],
-                    # 'area': None,
-                    # 'fuel_type': record['fuel'],
-                    # 'begin': datetime.datetime.fromtimestamp(record['begin'] / 1000).strftime("%Y-%m-%dT%H:%M:%S"),
-                    # 'end': datetime.datetime.fromtimestamp(record['end'] / 1000).strftime("%Y-%m-%dT%H:%M:%S"),
-                    # 'mw_cap': str(record['energy_limitation']).replace(',', ''),
-                    # 'mw_available': None,
-                    # 'status': record['canceled'],
-                    # 'pub_ts': datetime.datetime.fromtimestamp(record['publish_timestamp'] / 1000).strftime("%Y-%m-%dT%H:%M:%S"),
-                    # 'comment': record['reason']
                     'parse_date': parse_date,
                     'type': record['type'],
                     'company': record['short_name'],
@@ -294,79 +267,9 @@ class EexTransparencySpider(scrapy.Spider):
             WebDriverWait(self.driver, 5).until(EC.presence_of_element_located((By.XPATH, ts_xpath)))
             return True
         except TimeoutException:
-            # try:
-            #     error_element = self.driver.find_element_by_xpath('//div[@data-ng-show="noData && !loading && filterActive != false"]')
-            #     error_text = error_element.find_element_by_tag_name('p').text
-            #     if "There is no data reported"
             print("ERROR: Page load timeout or no data reported.")
-
             return False
 
-
-    # def parse_current(self, url):
-    #     """
-    #     Parses current availability page.
-    #
-    #     :param response: Response object
-    #     :return: Yields availability item dicts.
-    #     """
-    #
-    #     self.cur_date = datetime.datetime.utcnow()
-    #
-    #     retries = 0
-    #     print("Getting current unavailability page for:")
-    #     print(url)
-    #     self.driver.get(url)
-    #
-    #     current_page = 1
-    #
-    #     try:
-    #         WebDriverWait(self.driver, 20).until(EC.presence_of_element_located((By.XPATH, '//div[@class="timestamp"]')))
-    #     except TimeoutException:
-    #         print("Page load timeout or no data reported.")
-    #         return None
-    #
-    #     initial_page = self.driver.page_source
-    #     soup = BeautifulSoup(initial_page)
-    #
-    #     # Try to get the page count
-    #     pagecount = 0
-    #     while retries < 3:
-    #         try:
-    #             pagecount = len(soup.find('ul', {'class': 'pager clearfix'}).find_all('li'))
-    #             retries=0
-    #         except AttributeError:
-    #             pagecount=1
-    #
-    #         if pagecount == 0:
-    #             print("No data loaded. Retrying...")
-    #             self.driver.refresh()
-    #             retries += 1
-    #             continue
-    #         break
-    #
-    #     while current_page <= pagecount:
-    #         if pagecount > 1:
-    #             page_selected = self.select_page(current_page)
-    #             if not page_selected:
-    #                 return None
-    #
-    #         time.sleep(1)
-    #         ts_xpath = '//div[@class="timestamp"]'
-    #         try:
-    #             WebDriverWait(self.driver, 15).until(EC.presence_of_element_located((By.XPATH, ts_xpath)))
-    #         except TimeoutException:
-    #             print("Page load timeout. Retrying.")
-    #             retries += 1
-    #             self.driver.refresh()
-    #             continue
-    #
-    #         page_data =self.extract_rows(self.driver.page_source)
-    #
-    #         retries = 0
-    #         for item in page_data:
-    #             yield item
-    #         current_page += 1
     def parse_current(self, url):
         self.driver.get(url)
 
@@ -392,78 +295,6 @@ class EexTransparencySpider(scrapy.Spider):
             self.scrape_info['loaded_dates'].append(self.real_date.strftime('%Y-%m-%d'))
             with open(self.scrape_log, mode='w+') as log_file:
                 json.dump(self.scrape_info, log_file, indent=4)
-
-    # def select_page(self, number):
-    #     retries = 0
-    #     newXpath = '//ul[@class="pager clearfix"]/li/a[text()="{0}"]'.format(number)
-    #
-    #     page_number_loaded = False
-    #
-    #     while retries < 3:
-    #         try:
-    #             print("Waiting page number element: ", number)
-    #             WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.XPATH, newXpath)))
-    #             page_number_loaded = True
-    #             break
-    #         except TimeoutException:
-    #             retries += 1
-    #             print("Page load timeout. Try ", retries)
-    #             self.driver.refresh()
-    #             continue
-    #
-    #     if not page_number_loaded:
-    #         return False
-    #
-    #     page_element = self.driver.find_element_by_xpath(newXpath)
-    #
-    #     page_element.click()
-    #     time.sleep(2)
-    #     print('Waiting clicked page...')
-    #     try:
-    #         page_xPath = ('//ul[@class="pager clearfix"]/li/'
-    #                       'a[text()="{0}" and @class="ng-binding active"]').format(number)
-    #         WebDriverWait(self.driver, 20).until(EC.presence_of_element_located((By.XPATH, page_xPath)))
-    #         return True
-    #     except TimeoutException:
-    #         print("Page load timeout.")
-    #
-    #     print("ERROR: Unable to parse current availabilities.")
-    #     print(self.driver.current_url)
-    #     return False
-
-    # def extract_rows(self, page_html):
-    #     """
-    #     Extracts rows from page table to items.
-    #     :param page_html:
-    #     :return: Yields availability items.
-    #     """
-    #     page_soup = BeautifulSoup(page_html)
-    #     table = page_soup.find('table', {'data-table': ''}).find('tbody')
-    #     rows = table.find_all('tr')
-    #
-    #     for row in rows:
-    #         data = row.find_all('td')
-    #         try:
-    #             publish_timestamp = datetime.datetime.strptime(data[11].find(text=True).strip(), '%Y/%m/%d %H:%M')
-    #             begin_timestamp   = datetime.datetime.strptime(data[6].find(text=True).strip(), '%Y/%m/%d %H:%M')
-    #             end_timestamp     = datetime.datetime.strptime(data[7].find(text=True).strip(), '%Y/%m/%d %H:%M')
-    #             item = {
-    #                 'parse_date': self.cur_date,
-    #                 'pub_ts'   : publish_timestamp.isoformat(),
-    #                 'type'     : data[0].find(text=True).strip(),
-    #                 'unitname' : data[3].find(text=True).strip(),
-    #                 'area'     : data[5].find(text=True).strip(),
-    #                 'fuel_type': data[4].find(text=True).strip(),
-    #                 'begin'    : begin_timestamp.isoformat(),
-    #                 'end'      : end_timestamp.isoformat(),
-    #                 'mw_cap'   : data[8].find(text=True).strip(),
-    #                 'mw_available': None,
-    #                 'status'   : data[10].find(text=True).strip(),
-    #                 'comment'  : data[9].find(text=True).strip()
-    #             }
-    #             yield item
-    #         except IndexError:
-    #             continue
 
 
 class ScrapeJS(object):
